c79be14d891d4fae6c41511dc701a94d8c38fe80
vontv <vontv@es107m06.dhcp.umsl.edu>
2019-10-03 13:39:50 -0500

    calling display graphics

--- concordance.py
+++ concordance.py
@@ -23,7 +23,7 @@ import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 
-def readInput():
+#def readInput():
 
 
 
@@ -41,7 +41,7 @@ def readInput():
 
 # BuildList() will takes in a list as a parameter and should count word occurrences 
 
-def buildList():
+#def buildList():
 
 
 
@@ -53,7 +53,7 @@ def buildList():
 
 
 
-def writeListToFile():
+#def writeListToFile():
 
 
 
@@ -73,6 +73,16 @@ def writeListToFile():
 
 
 def displayGraphic():
+    objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
+    y_pos = np.arange(len(objects))
+    performance = [10,8,6,4,2,1]
+
+    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('Usage')
+    plt.title('Programming language usage')
+
+    plt.show()
 
 
 
@@ -89,14 +99,13 @@ def displayGraphic():
 
 
 
-
-#Main function
+#Main function (python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose)
 def startApp():
-    wordList = readInput() #returns an unsorted wordlist ["word1", "word2", "word3"]
-    sortedCountedList = buildList(wordList) #returns a sorted array with counts [["word1", 10], ["word2", 5], ["word3", 2]]
-    writeListToFile(sortedCountedList) #writes file to list
+    #wordList = readInput() #returns an unsorted wordlist ["word1", "word2", "word3"]
+    #sortedCountedList = buildList(wordList) #returns a sorted array with counts [["word1", 10], ["word2", 5], ["word3", 2]]
+    #writeListToFile(sortedCountedList) #writes file to list
 
-    displayGraphic(sortedCountedList) #displays graphic
+    displayGraphic("""sortedCountedList""") #displays graphic
 
 if __name__ == "__main__":
     startApp()

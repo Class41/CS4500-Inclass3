1a7905003260425f819157f67ad8a1f030d8e7f9
daj356 <daj356@mst.edu>
2019-10-03 12:38:20 -0500

    Added a few comments, created new .py

---
+++ concordance.py
@@ -0,0 +1,10 @@
+# Authors: Daniel Janis, Joel Ascher, Vasyl Onufriyev
+#
+# Prompt the user for name of external textfile
+# --> If the file doesn't exist, prompt descriptive error
+#       and reprompt
+#
+# When textfile is opened, build a list of words that appear
+# in the file. (using an unsorted array)
+# --> Sort list into alphabetical order
+# -->

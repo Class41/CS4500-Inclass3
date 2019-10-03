409b15ed0fbdaf7fc3057ec35306cca7a1edd571
vontv <vontv@es107m06.dhcp.umsl.edu>
2019-10-03 13:00:46 -0500

    added layout

--- concordance.py
+++ concordance.py
@@ -12,12 +12,79 @@
 """
 1. Begin (startApp()) will be main loop
 2. Ask for textfile name (readInput()) should handle errors/reprompt
-3. Return from readInput() and begin building a list of words (readAndBuild()) using unsorted array
-4. Return from readAndBuild(), pass into sorting algorithm
+3. Return from readInput() and begin building a list of words (buildList()) using unsorted array
+4. Return from buildList(), pass into sorting algorithm
 5. Build a list in the following format: Word #oftimesappeared (buildList())
-6. Write the output of buildList() to file named: WordCounts.txt, overwrite if exists/create new if not exist
+6. Write the output of buildList() to file named: WordCounts.txt, overwrite if exists/create new if not exist (writeListToFile())
 7. Display bargraph of read in data. Resolve ties (displayGraphic())
 """
+
+
+def readInput():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def buildList():
+
+
+
+
+
+
+
+
+
+
+
+def writeListToFile():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def displayGraphic():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

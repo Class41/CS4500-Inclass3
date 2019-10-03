8eaa4842a428ac97531469c9d8206e18866dc3a4
vontv <vontv@es107m06.dhcp.umsl.edu>
2019-10-03 12:56:02 -0500

    added logic flow for program

--- concordance.py
+++ concordance.py
@@ -9,6 +9,18 @@
 # --> Sort list into alphabetical order
 # -->
 
+"""
+1. Begin (startApp()) will be main loop
+2. Ask for textfile name (readInput()) should handle errors/reprompt
+3. Return from readInput() and begin building a list of words (readAndBuild()) using unsorted array
+4. Return from readAndBuild(), pass into sorting algorithm
+5. Build a list in the following format: Word #oftimesappeared (buildList())
+6. Write the output of buildList() to file named: WordCounts.txt, overwrite if exists/create new if not exist
+7. Display bargraph of read in data. Resolve ties (displayGraphic())
+"""
+
+
+
 def startApp():
     print("working")
 
